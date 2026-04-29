f3542021f505b6cc5ec520392bfdee48b284741d
LucarioWolf <alan.ruelas812@gmail.com>
2026-04-28 21:30:42 -0700

    Debugging and fix JSON bugs

--- PlaywriteTest.py
+++ PlaywriteTest.py
@@ -1,7 +1,7 @@
-from playwright.sync_api import sync_playwright, Playwright
+from playwright.sync_api import sync_playwright, Playwright, TimeoutError as PlaywrightTimeoutError
 import json
 
-globalIndex = 0;
+globalIndex = 0
 
 def run(playwright: Playwright):
     
@@ -14,6 +14,7 @@ def run(playwright: Playwright):
     chromium = playwright.chromium
     browser = chromium.launch()
     page = browser.new_page()
+    page.set_default_timeout(3000)
 
     #Go to professor google scholar
     page.goto("https://scholar.google.com/citations?user=k12ONA8AAAAJ&hl=en&oi=ao")
@@ -21,14 +22,12 @@ def run(playwright: Playwright):
     #Gets the screenshot of the page
     page.screenshot(path="example.png")
 
-    """
-    for row in page.get_by_role("link").all():
-        print(row.text_content())
-    """
+    reloadPage(page)
 
     #All titles in link
     articleList = []
 
+    
     #for all table data that has a title, get the title text and add it to list
     for row in page.locator(titleCell).filter(has=page.get_by_role("link")).all():
         title = row.get_by_role("link").text_content()
@@ -37,23 +36,12 @@ def run(playwright: Playwright):
 
     articleList.remove("Title")
 
-    createJsonFile(page, articleList[0])
-
-    """
-    page.get_by_role("link", name= articleList[0]).click()
-
-    authors = getDivValues(page, "Authors")
-    date = getDivValues(page, "Publication date")
-    publisher = getDivValues(page, "Publisher")
-    description = getDivValues(page,"Description")
-
-    print("Title: " + articleList[0])
-    print("Authors: " + authors)
-    print("Date: " + date)
-    print("Publisher: " + publisher)
-    print("Description: " + description)
-    """
-
+    for currentArticle in articleList:
+        createJsonFile(page, currentArticle)
+        print("Article " + str(globalIndex) + ": " + currentArticle)
+    
+    
+    
     page.screenshot(path="thelink.png")
 
     #Gets the HTMl of the page
@@ -73,23 +61,32 @@ def getDivValues(page, textLocator):
     #get the div containing the data
     currentDiv = page.locator(articleDiv).filter(
         has=page.locator(fieldDiv)
-        .and_(page.get_by_text(textLocator))
+        .and_(page.get_by_text(textLocator, exact=True))
     )
 
     #Extract the data
-    textValue = currentDiv.locator(valueDiv).text_content()
+    try:
+        textValue = currentDiv.locator(valueDiv).text_content()
+    except PlaywrightTimeoutError:
+        return None
 
     #Return data
     return textValue
 
+#Create a json file of an article
 def createJsonFile(page, currentTitle):
     
+    global globalIndex
 
     #Click to the link with the title
-    page.get_by_role("link", name = currentTitle).click()
+    page.get_by_role("link", name = currentTitle, exact=True).first.click()
 
     #Get all nesscary data
-    authors = getDivValues(page, "Authors").split(", ")
+    authors = getDivValues(page, "Authors")
+
+    if(authors != None):
+        authors = authors.split(", ")
+
     date = getDivValues(page, "Publication date")
     publisher = getDivValues(page, "Publisher")
     description = getDivValues(page,"Description")
@@ -108,18 +105,22 @@ def createJsonFile(page, currentTitle):
         "cites" : [],
         "summary" : description
     }
-    with open("articles.json", "w", encoding="utf-8") as articleFile:
+    with open("articles.json", "a", encoding="utf-8") as articleFile:
         json.dump(newJson, articleFile, indent=4)
 
+    globalIndex += 1
 
-    """
-    print("Title: " + currentTitle)
-    print("Authors: " + authors)
-    print("Date: " + date)
-    print("Publisher: " + publisher)
-    print("Description: " + description)
-    """
+    page.go_back()
+    reloadPage(page)
+    
 
+def reloadPage(page):
+    showMoreButton = page.get_by_role("Button", name="Show More")
+    while(showMoreButton.is_enabled()):
+        showMoreButton.click(no_wait_after=False)
+        page.wait_for_timeout(1000);
+    
+    
 
 with sync_playwright() as playwright:
     run(playwright)
